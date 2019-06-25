f46fa93aaec9b92a10895d34fe53e5e175d29065
Rodrigo Pinheiro <rpinheir@thoughtworks.com>
2019-06-24 23:30:38 -0300

    Creating integration tests structure

--- src/test/java/com/sparke/networks/personality/PersonalityApplicationTests.java
+++ src/test/java/com/sparke/networks/personality/PersonalityApplicationTests.java
@@ -1,16 +1,74 @@
 package com.sparke.networks.personality;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import com.mongodb.BasicDBObjectBuilder;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+import de.flapdoodle.embed.mongo.MongodExecutable;
+import de.flapdoodle.embed.mongo.MongodStarter;
+import de.flapdoodle.embed.mongo.config.IMongodConfig;
+import de.flapdoodle.embed.mongo.config.MongodConfigBuilder;
+import de.flapdoodle.embed.mongo.config.Net;
+import de.flapdoodle.embed.mongo.distribution.Version;
+import de.flapdoodle.embed.process.runtime.Network;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@ContextConfiguration(classes = PersonalityApplication.class)
+@DataMongoTest
+@ExtendWith(SpringExtension.class)
 public class PersonalityApplicationTests {
 
-	@Test
-	public void contextLoads() {
+    private MongodExecutable mongodExecutable;
+    private MongoTemplate mongoTemplate;
+
+    @BeforeEach
+    void setUp() throws IOException {
+		String ip = "localhost";
+		int port = 27017;
+
+		IMongodConfig mongodConfig = new MongodConfigBuilder().version(Version.Main.PRODUCTION)
+				.net(new Net(ip, port, Network.localhostIsIPv6()))
+				.build();
+
+		MongodStarter starter = MongodStarter.getDefaultInstance();
+		mongodExecutable = starter.prepare(mongodConfig);
+		mongodExecutable.start();
+		mongoTemplate = new MongoTemplate(new MongoClient(ip, port), "test");
 	}
+
+    @AfterEach
+    void clean() {
+        mongodExecutable.stop();
+    }
+
+    @DisplayName("given object to save"
+            + " when save object using MongoDB template"
+            + " then object is saved")
+    @Test
+    public void test() throws Exception {
+        // given
+        DBObject objectToSave = BasicDBObjectBuilder.start()
+                .add("key", "value")
+                .get();
+
+        // when
+        mongoTemplate.save(objectToSave, "collection");
+
+        // then
+        assertThat(mongoTemplate.findAll(DBObject.class, "collection")).extracting("key")
+                .containsOnly("value");
+    }
 
 }
